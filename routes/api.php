cd260b7d51107e2d342e6bc9ad51bb0765f55cd4
Bruno Lima <brunoluislima19@gmail.com>
2024-02-07 21:20:23 -0300

    feat: route private

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\UserAuthenticationController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,16 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 
-Route::get('products', [ProductsController::class, 'index'])->name('products.index');
-Route::get('products/{product}', [ProductsController::class, 'show'])->name('products.show');
-Route::post('products', [ProductsController::class, 'store'])->name('products.store');
-Route::put('products/{product}', [ProductsController::class, 'update'])->name('products.update');
-Route::delete('products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
+Route::prefix('v1')->group(function (){
+    Route::post('login', [UserAuthenticationController::class, 'login']);
+    Route::post('register', [UserAuthenticationController::class, 'register']);
+});
 
-Route::post('login', [UserAuthenticationController::class, 'login']);
-Route::post('register', [UserAuthenticationController::class, 'register']);
-Route::post('logout', [UserAuthenticationController::class, 'logout'])->middleware('auth:sanctum');
+Route::prefix('v1')->middleware('auth:sanctum')->group(function (){
+    Route::get('products', [ProductsController::class, 'index'])->name('products.index');
+    Route::get('products/{product}', [ProductsController::class, 'show'])->name('products.show');
+    Route::post('products', [ProductsController::class, 'store'])->name('products.store');
+    Route::put('products/{product}', [ProductsController::class, 'update'])->name('products.update');
+    Route::delete('products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
+
+    Route::post('logout', [UserAuthenticationController::class, 'logout'])->middleware('auth:sanctum');
+});
